2c0d2473cedf908aafd14b3585c2f0aae1f19722
shakilkhan95 <th.shakilkhan@gmail.com>
2026-04-10 05:25:41 +0400

    Product data from form displaying in ui

--- src/components/ProductManagement/ProductTable.jsx
+++ src/components/ProductManagement/ProductTable.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
 const ProductTable = ({products}) => {
-    console.log(products)
+    
   return (
     <div>
         <h3>Products: {products.length}</h3>
+        <table style={{width: '240px', margin: '0 auto', padding: '20px'}}>
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {products.map((product, index) =>
+                    <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{product.productName}</td>
+                        <td>{product.price}</td>
+                        <td>{product.quantity}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
     </div>
   )
 }
