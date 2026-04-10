b34afe9f6910299566e867a8d3a1fc6f1b81b065
shakilkhan95 <th.shakilkhan@gmail.com>
2026-04-10 05:36:00 +0400

    Added form validation functionality

--- src/components/ProductManagement/ProductForm.jsx
+++ src/components/ProductManagement/ProductForm.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ProductForm = ({ handleAddProducts }) => {
+
+    const [error, setError] = useState('');
+
   const handleProductSubmit = (e) => {
     e.preventDefault();
     const productName = e.target.productName.value;
     const price = e.target.price.value;
     const quantity = e.target.quantity.value;
+
+    if(productName.length <= 0){
+        setError('Please write a product name');
+        return
+    } else if(price.length <= 0){
+        setError("Please give product price");
+        return;
+    }else if(quantity <= 0){
+        setError("Provide at least 1 as quantity");
+        return;
+    } else{
+        setError('');
+    }
 
     const newProduct = { productName, price, quantity };
     handleAddProducts(newProduct);
@@ -22,6 +38,7 @@ const ProductForm = ({ handleAddProducts }) => {
         <br />
         <input type="submit" value="Add Product" />
       </form>
+      <p style={{textAlign: 'center', color: 'red'}}><small>{error}</small></p>
     </div>
   );
 };
